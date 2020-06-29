c0da31ba02eb07e0301692470c1481771823d622
Gregor Zeller <Gregor.Zeller@outlook.com>
2020-06-28 17:24:21 -0700

    Updates

---
+++ precache-manifest.28d27481fe52e9fdde4bb822ee3f86cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7971475eea4706c4dacb1574bc1d76bd",
+    "url": "/AlgoProject/index.html"
+  },
+  {
+    "revision": "d2223f18e7c3c579f4d6",
+    "url": "/AlgoProject/static/css/main.26e2120c.chunk.css"
+  },
+  {
+    "revision": "f206cfd503d98854d1bb",
+    "url": "/AlgoProject/static/js/2.1b5839bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlgoProject/static/js/2.1b5839bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2223f18e7c3c579f4d6",
+    "url": "/AlgoProject/static/js/main.0f7d9d7a.chunk.js"
+  },
+  {
+    "revision": "5d7157e25aa36869839c",
+    "url": "/AlgoProject/static/js/runtime-main.61bfec41.js"
+  }
+]);
